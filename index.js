e319f30dec5db74ce929aa67b7a88d0bb4fc7ad5
David Luu <dluu.audeo@gmail.com>
2022-01-21 18:51:09 -0800

    created prompts for description and created template literal on template.js

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+
+//project title
+//description
+//installation instructions
+//usage information
+//license
+//contributing
+//tests
+//questions
 
 inquirer
   .prompt([
     {
       type: 'input',
-      message: 'What is your user name?',
-      name: 'username',
+      message: 'What is your project title?',
+      name: 'title',
     },
     {
-      type: 'password',
-      message: 'What is your password?',
-      name: 'password',
+      type: 'input',
+      message: 'What was your motivation?',
+      name: 'motivation',
+    },
+    {
+      type: 'input',
+      message: 'Why did you build this project?',
+      name: 'reason',
+    },
+    {
+      type: 'input',
+      message: 'What problem does it solve?',
+      name: 'problem',
+    },
+    {
+      type: 'input',
+      message: 'What did you learn?',
+      name: 'lesson',
     },
     {
       type: 'password',
